e98b4eb386f021ed59362e50cf979c0497924aa7
Saumil Shah <36410221+ImSaumilShah@users.noreply.github.com>
2018-04-23 11:30:03 +0530

    Update APIUtils.java

--- app/src/main/java/com/saumil/apidemo/Retrofit/Data/Remote/APIUtils.java
+++ app/src/main/java/com/saumil/apidemo/Retrofit/Data/Remote/APIUtils.java
@@ -7,7 +7,7 @@ package com.saumil.apidemo.Retrofit.Data.Remote;
 
 public class APIUtils {
 
-    public static final String BASE_URL = "http://server.midasshipping.com/api/";
+    public static final String BASE_URL = "Your_Base_URL";
 
     public static APIService getAPIService() {
         return RetrofitClient.getClient(BASE_URL).create(APIService.class);
